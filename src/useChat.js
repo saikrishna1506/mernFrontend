f5a8c1802a52f7565fd780e3ea97f2c95d69b194
saikrishna1506 <108617880+saikrishna1506@users.noreply.github.com>
2023-10-22 18:20:40 +0530

    Update useChat.js

--- src/useChat.js
+++ src/useChat.js
@@ -1,35 +1,44 @@
-import {useEffect,useRef,useState,UseState} from "react"
-import socketIOClient from 'socket.io-client'
+import { useEffect, useRef, useState } from "react";
+import socketIOClient from "socket.io-client";
 
-const NEW_CHAT_MESSAGE_EVENT="newChatMessage"
-const SOCKET_SERVER_URL="https://chatbackend-2qo9.onrender.com"
+const NEW_CHAT_MESSAGE_EVENT = "newChatMessage";
+const SOCKET_SERVER_URL = "https://chatbackend-2qo9.onrender.com/4000";
 
-const useChat =(roomId)=>{
-    const [messages,setMessages] =useState([])
-    const socketRef =useRef()
-    useEffect(()=>{
-        socketRef.current=socketIOClient(SOCKET_SERVER_URL,{
-            query:{roomId}
-        })
-        socketRef.current.on(NEW_CHAT_MESSAGE_EVENT,(message)=>{
-            const incomingMessage={
-                ...message,
-                ownedByCurrentUser:message.senderId === socketRef.current.id
-            }
-            setMessages((messages)=>[...messages,incomingMessage])
-        })
-        return ()=>{
-            socketRef.current.disconnect();
-        }
-    },[roomId])
-    const sendMessage =(messageBody)=>{
-        socketRef.current.emit(NEW_CHAT_MESSAGE_EVENT,{
-            body:messageBody,
-            senderId:socketRef.current.id
-        })
+const useChat = (roomId) => {
+  const [messages, setMessages] = useState([]);
+  const socketRef = useRef();
+
+  useEffect(() => {
+    try {
+      socketRef.current = socketIOClient(SOCKET_SERVER_URL, {
+        query: { roomId }
+      });
+      socketRef.current.on(NEW_CHAT_MESSAGE_EVENT, (message) => {
+        const incomingMessage = {
+          ...message,
+          ownedByCurrentUser: message.senderId === socketRef.current.id
+        };
+        setMessages((messages) => [...messages, incomingMessage]);
+      });
+    } catch (error) {
+      console.error("WebSocket connection error:", error);
     }
-    // console.log(messages[0])
-    return {messages,sendMessage}
-    
-}
+
+    return () => {
+      if (socketRef.current) {
+        socketRef.current.disconnect();
+      }
+    }
+  }, [roomId]);
+
+  const sendMessage = (messageBody) => {
+    socketRef.current.emit(NEW_CHAT_MESSAGE_EVENT, {
+      body: messageBody,
+      senderId: socketRef.current.id
+    });
+  };
+
+  return { messages, sendMessage };
+};
+
 export default useChat;
